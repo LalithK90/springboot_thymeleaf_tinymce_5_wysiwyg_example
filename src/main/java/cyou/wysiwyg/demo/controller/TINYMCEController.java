c7693d8bffe4868dea0694b82a88a01a3a115545
lalithk90 <asakahatapitiya@gmail.com>
2023-06-11 14:15:44 +0530

    template plugin config and theme was settled

--- src/main/java/cyou/wysiwyg/demo/controller/TINYMCEController.java
+++ src/main/java/cyou/wysiwyg/demo/controller/TINYMCEController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TINYMCEController {
     private final DemoService demoService;
 
+
     @GetMapping("/tinymce")
     public String tinymce() {
         return "tinymce";
